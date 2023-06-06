0aba827f13d00a896e812f463023da52bb289292
Roman <roma.klimov10.94@gmail.com>
2023-06-06 13:04:20 +1100

    Промежуточно завершенная задача 01 , необходима дороботка на проверку не вещественных чиесл , и других символов, перехожу к решению задачи 2

--- descending (recursion)_task01/Program.cs
+++ descending (recursion)_task01/Program.cs
@@ -4,4 +4,21 @@
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
 
-int 
+int Descending (int n)
+{
+    Console.WriteLine(n);
+    if(n <= 0 ) return -1; 
+    else return Descending(n -1);
+}
+
+int Prompt(string massage)
+{
+    Console.WriteLine(massage);
+    string string_input = Console.ReadLine()!;
+    int result = int.Parse(string_input);
+    return result;
+}
+
+
+int number = Prompt("Введите натуральное число");
+Descending(number);
