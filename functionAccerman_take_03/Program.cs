567de47b947b063d52a0a753b46400132b9b4403
Roman <roma.klimov10.94@gmail.com>
2023-06-06 17:23:53 +1100

    Создал программу вычесления функции Аккермана

--- functionAccerman_take_03/Program.cs
+++ functionAccerman_take_03/Program.cs
@@ -2,3 +2,18 @@
 m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29*/
 
+int AccermanFunction(int m, int n)
+{
+   if(m == 0) 
+      return n + 1;
+   else if(m != 0 && n == 0)
+   {
+      return AccermanFunction(m - 1, 1);
+   }
+   return AccermanFunction(m -1, AccermanFunction(m, n - 1)); 
+}
+
+
+int m = 3;
+int n = 2;
+Console.WriteLine($"функция Аккермана = {AccermanFunction(m, n)}");
